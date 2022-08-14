d27403a10aee4e4e3a7695c48cff11a92e59faa2
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-14 14:54:06 +0300

    Update config.py

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,6 +1,6 @@
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-REDIRECT_URI = "http://127.0.0.1:5000/login"
+CLIENT_ID = "994209392566210560"
+CLIENT_SECRET = "dOIjqBEGLgaSuxqSvHCIprbPEkxYiePI"
+REDIRECT_URI = "http://goldybot.gq"
 
 DEFAULT_URL = "https://discord.com/api"
 SCOPES = "identify%20email"
